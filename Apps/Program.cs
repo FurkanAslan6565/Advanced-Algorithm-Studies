45840465a6bafc64ecdebbdec84dcf79c1ef375c
Furkan Aslan <murannas.65@gmail.com>
2022-07-14 18:16:20 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic; 
+
+namespace Apps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new DataStructures
+                .Array
+                .Array<int>();
+            arr.Add(23);
+            arr.Add(55);
+            arr.Add(44);
+            Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+
+
+
+
+
+
+           /* // Array 
+            char[] arrChar = new char[3];
+            // arrayList
+            //List < T >
+            var arrInt = new List<int>(); // int değer olduğu için strşng değer eklenemez 
+            arrInt.Add(10); // Eklemek  
+            arrInt.AddRange(new int[] {1,2,3}) ;
+            // arrInt.RemoveAt(0) ; // 0 elemanı çıkar  
+            // Kodu yazdırma 
+            foreach (var item in arrInt)
+            {
+                Console.WriteLine(item); 
+
+            }*/
+            Console.ReadKey();
+                
+            
+        }
+    }
+}
---
+++ DataStructures/Array/Array.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    public class Array<T> : IEnumerable<T> , ICloneable 
+    {
+        private T[] InnerList;
+        public int Count { get;  private set; }
+        public int Capacity => InnerList.Length; // Koleksiyon kapasitesi dizinin uzunluğuna eşittir.
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0; 
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(T item)  //Diziye bir eleman eklemek item üzerin de
+        {
+            if (InnerList.Length == Count) 
+            {
+                DoubleArray();
+            }
+            InnerList[Count] = item;
+            Count++; // bir sonraki elemanı dizinin 1. gözüne yerleştirebilirsin
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length *2];
+            for (int i = 0; i < InnerList.Length; i++)
+            {
+                temp[i] = InnerList[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
